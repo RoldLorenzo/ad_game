4acaf854b19a6aab907d3653806d08740b86ce76
João Augusto <joaoalt0502@gmail.com>
2025-05-31 15:32:09 -0300

    adicionado personagem rogue e animações

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,22 @@
 extends CharacterBody3D
 
+@onready var animation_tree: AnimationTree = $AnimationTree
+
 @export var speed = 10
 @export var bullet_scene: PackedScene
 
+func _ready() -> void:
+	pass
+
 func _physics_process(delta: float) -> void:
 	var direction = Vector3.ZERO
+	animation_tree.set("parameters/Transition/transition_request", "Run")
 	
 	if Input.is_action_pressed("move_right"):
+		animation_tree.set("parameters/Transition/transition_request", "RunRight")
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
+		animation_tree.set("parameters/Transition/transition_request", "RunLeft")
 		direction.x -= 1
 	
 	if direction != Vector3.ZERO:
